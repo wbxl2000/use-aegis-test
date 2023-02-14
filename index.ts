50da6ee36cdfecb9a1e60c62b84a98e4ccc50431
longyuqi <longyuqi@tencent.com>
2023-02-14 22:29:33 +0800

    feat: add error code

--- index.ts
+++ index.ts
@@ -1,16 +1,5 @@
-import Aegis from "aegis-web-sdk";
+import myUI from "./my-ui.vue"
 
-const aegis = new Aegis({
-  id: "random",
-  reportApiSpeed: true, // 接口测速
-  reportAssetSpeed: true, // 静态资源测速
-  version: "1.4.1",
-});
-
-export const loginSuccess = () =>  {
-  aegis.reportEvent({
-    name: "login",
-    ext1: "login-success",
-    ext2: "webTUICallKit",
-  });
-};
+export {
+  myUI
+}
---
+++ my-ui.vue
@@ -0,0 +1,9 @@
+<script lang="ts" setup>
+import { TUICallType } from "tuicall-engine-webrtc";
+
+console.log(TUICallType);
+</script>
+
+<template>
+	<div></div>
+</template>
