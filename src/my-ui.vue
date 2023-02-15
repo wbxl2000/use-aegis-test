3d81933a36ea6f1c7437ec6555a28d93ddbd14f8
longyuqi <longyuqi@tencent.com>
2023-02-15 14:53:27 +0800

    feat: add aegis

--- src/my-ui.vue
+++ src/my-ui.vue
@@ -1,9 +1,26 @@
 <script lang="ts" setup>
 import { TUICallType } from "tuicall-engine-webrtc";
+import Aegis from "aegis-web-sdk";
 
 console.log(TUICallType);
+
+const aegis = new Aegis({
+  id: "iHWefAYqDNAiuxNMQZ",
+  reportApiSpeed: true, // 接口测速
+  reportAssetSpeed: true, // 静态资源测速
+  version: "1.4.1",
+});
+
+function loginSuccess(SDKAppID: number) {
+  aegis.reportEvent({
+    name: "login",
+    ext1: "login-success",
+    ext2: "webTUICallKit",
+    ext3: SDKAppID.toString()
+  });
+};
 </script>
 
 <template>
-	<div></div>
+	<div @click="loginSuccess(123)"></div>
 </template>
